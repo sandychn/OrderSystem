00f13ad739faf60054988517b2d0db1de35b9230
Ming996 <571195325@qq.com>
2020-10-15 15:19:43 +0800

    实体类的修改

--- src/main/java/com/example/common/JdbcUtil.java
+++ src/main/java/com/example/common/JdbcUtil.java
@@ -3,21 +3,21 @@ package com.example.common;
 import java.sql.*;
 
 public class JdbcUtil {
-    public static Connection connection = null;
+    private static Connection connection = null;
 
-    public static void getConnection() throws Exception{
+    public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.jdbc.Driver");//加载数据库驱动
 
-        String url = "jdbc:mysql://localhost/mydatabase?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8";
+        String url = "jdbc:mysql://localhost/ordering?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8";
         String user = "root";
         String password = "281215";
         if(connection == null){
             connection = DriverManager.getConnection(url, user, password);
         }
-
+        return connection;
 
     }
-    public static void close(ResultSet rs, Statement st, Connection conn) {
+    public static void close(ResultSet rs, Statement st) {
         try {
             if (rs != null) {
                 rs.close();
@@ -31,14 +31,6 @@ public class JdbcUtil {
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
-            } finally {
-                if (conn != null) {
-                    try {
-                        conn.close();
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                }
             }
         }
     }

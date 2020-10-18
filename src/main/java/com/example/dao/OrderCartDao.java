b86e3d73f568fd6f14af8d19a09ed0cbbd543cb5
Ming996 <571195325@qq.com>
2020-10-18 22:14:45 +0800

    update OrderCartDao.java, OrderDao.java, OrderDetailDao.java

--- src/main/java/com/example/dao/OrderCartDao.java
+++ src/main/java/com/example/dao/OrderCartDao.java
@@ -1,4 +1,15 @@
 package com.example.dao;
 
+import com.example.common.Status;
+import com.example.common.SystemException;
+import com.example.pojo.OrderCart;
+
+import java.util.List;
+
 public interface OrderCartDao {
+    boolean isExist(String userID,String foodID) throws SystemException;
+    Status addOrderCart(OrderCart orderCart) throws SystemException;
+    Status delete(String userID,String foodID) throws SystemException;
+    Status update(OrderCart orderCart) throws SystemException;
+    List<OrderCart> getOrderCarts(String userID) throws SystemException;
 }
--- src/main/java/com/example/dao/OrderDetailDao.java
+++ src/main/java/com/example/dao/OrderDetailDao.java
@@ -1,4 +1,15 @@
 package com.example.dao;
 
+import com.example.common.Status;
+import com.example.common.SystemException;
+import com.example.pojo.OrderDetail;
+
+import java.util.List;
+
 public interface OrderDetailDao {
+    List<OrderDetail> getOrderDetails(String orderID) throws SystemException;  //获取当前订单所有菜品
+    Status addOrderDetail(OrderDetail orderDetail) throws SystemException;  //为指定的订单增加一道菜品
+    Status update(OrderDetail orderDetail) throws SystemException;          //修改订单中的菜品信息
+    Status delete(String orderID,String foodID) throws SystemException;     //删除订单中的一道菜品
+    boolean isExist(String orderID,String foodID) throws SystemException;   //判断菜品是否存在于该订单
 }
